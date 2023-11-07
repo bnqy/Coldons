f96422d58977fd0d826fab485888112282ace903
Beneqi <obbonobo@gmail.com>
2023-11-07 17:06:22 +0600

    inserting

--- Northwind.Web/Pages/Suppliers.cshtml.cs
+++ Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,16 +1,23 @@
 using Microsoft.AspNetCore.Mvc.RazorPages; // PageModel
 using Coldons.Lib;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Northwind.Web.Pages;
 public class SuppliersModel : PageModel
 {
     private NorthwindContext db;
-    public SuppliersModel(NorthwindContext injectedContext)
+
+	[BindProperty]
+	public Supplier? Supplier { get; set; }
+
+	public IEnumerable<Supplier>? Suppliers { get; set; }
+
+	public SuppliersModel(NorthwindContext injectedContext)
     {
         db = injectedContext;
     }
 
-    public IEnumerable<Supplier>? Suppliers { get; set; }
+  
     public void OnGet()
     {
         ViewData["Title"] = "Northwind B2B - Suppliers";
@@ -19,4 +26,19 @@ public class SuppliersModel : PageModel
             .OrderBy(c => c.Country)
             .ThenBy(c => c.CompanyName);
     }
+
+	
+	public IActionResult OnPost()
+	{
+		if ((Supplier is not null) && ModelState.IsValid)
+		{
+			db.Suppliers.Add(Supplier);
+			db.SaveChanges();
+			return RedirectToPage("/suppliers");
+		}
+		else
+		{
+			return Page();
+		}
+	}
 }
