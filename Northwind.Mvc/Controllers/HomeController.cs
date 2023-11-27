030a402f295fc2e7d2ae918ee03eb6f68a0f1b32
Beneqi <obbonobo@gmail.com>
2023-11-27 22:20:45 +0600

    final async connect db

--- Northwind.Mvc/Controllers/HomeController.cs
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Northwind.Mvc.Models;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Coldons.Lib;
+using Microsoft.EntityFrameworkCore;
 
 namespace Northwind.Mvc.Controllers
 {
@@ -19,7 +20,7 @@ namespace Northwind.Mvc.Controllers
 		}
 
 		[ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any)]
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
 			_logger.LogError("This is a serious error (not really!)");
 			_logger.LogWarning("This is your first warning!");
@@ -29,20 +30,20 @@ namespace Northwind.Mvc.Controllers
 			HomeIndexViewModel model = new
 				(
 				VisitorCount: (new Random()).Next(1, 1001),
-				Categories: db.Categories.ToList(),
-				Products: db.Products.ToList()
+				Categories: await db.Categories.ToListAsync(),
+				Products: await db.Products.ToListAsync()
 				);
 			return View(model);
 		}
 
-		public IActionResult ProductDetail(int? id)
+		public async Task<IActionResult> ProductDetail(int? id)
 		{
 			if (!id.HasValue)
 			{
 				return BadRequest("You must pass a product ID in the route, for example, / Home / ProductDetail / 21");
 		    }
-			Product? model = db.Products
-			.SingleOrDefault(p => p.ProductId == id);
+			Product? model = await db.Products
+			.SingleOrDefaultAsync(p => p.ProductId == id);
 			if (model == null)
 			{
 
@@ -55,9 +56,18 @@ namespace Northwind.Mvc.Controllers
 		{
 			return View(); // the page with a form to submit
 		}
+
+		[HttpPost]
 		public IActionResult ModelBinding(Thing thing)
 		{
-			return View(thing); // show the model bound thing
+			HomeModelBindingViewModel model = new(thing,
+				!ModelState.IsValid,
+				ModelState.Values
+				.SelectMany(state => state.Errors)
+				.Select(error => error.ErrorMessage)
+				);
+
+			return View(model); // show the model bound thing
 		}
 
 
@@ -72,6 +82,27 @@ namespace Northwind.Mvc.Controllers
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult ProductsThatCostMoreThan(decimal? price)
+		{
+			if (!price.HasValue)
+			{
+				return BadRequest("You must pass a product price in the query string, for example, / Home / ProductsThatCostMoreThan ? price = 50");
+			}
+			IEnumerable<Product> model = db.Products
+				.Include(p => p.Category)
+				.Include(p => p.Supplier)
+				.Where(p => p.UnitPrice > price);
+
+			if (!model.Any())
+			{
+				return NotFound($"No products cost more than {price:C}.");
+			}
+
+			ViewData["MaxPrice"] = price.Value.ToString("C");
+
+			return View(model);
 		}
 	}
 }
--- Northwind.Mvc/Models/Thing.cs
+++ Northwind.Mvc/Models/Thing.cs
@@ -1,6 +1,13 @@
-﻿namespace Northwind.Mvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace Northwind.Mvc.Models;
 public class Thing
 {
+	[Range(1, 10)]
 	public int? Id { get; set; }
+
+	[Required]
 	public string? Color { get; set; }
+
+	[EmailAddress]
+	public string? Email { get; set; }
 }
