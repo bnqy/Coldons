d63b6609e68273d0eab227c44c10a86d228246c7
Beneqi <obbonobo@gmail.com>
2023-12-04 12:19:30 +0600

    create web service for northwind

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,8 +1,34 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Coldons.Lib; // AddNorthwindContext extension method
+
+using static System.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddNorthwindContext();
+builder.Services.AddControllers(options =>
+{
+	WriteLine("Default output formatters");
+	
+	foreach (IOutputFormatter formatter in options.OutputFormatters)
+	{
+		OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+		if (mediaFormatter == null)
+		{
+			WriteLine($" {formatter.GetType().Name}");
+		}
+		else
+		{
+			WriteLine(" {0}, Media types: {1}", 
+				arg0: mediaFormatter.GetType().Name, 
+				arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes));
+		}
+	}
+}).AddXmlDataContractSerializerFormatters()
+.AddXmlSerializerFormatters();
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
