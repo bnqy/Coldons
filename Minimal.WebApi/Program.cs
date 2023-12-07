10909fd57e7fb927b55d56dc6e640f0925193d19
Beneqi <obbonobo@gmail.com>
2023-12-07 16:37:32 +0600

    min api create

---
+++ Minimal.WebApi/Program.cs
@@ -0,0 +1,36 @@
+using Northwind.Common;
+namespace Minimal.WebApi;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		builder.WebHost.UseUrls("https://localhost:5003");
+		builder.Services.AddCors();
+
+		var app = builder.Build();
+
+		// only allow the MVC client and only GET requests
+		app.UseCors(configurePolicy: options =>
+		{
+			options.WithMethods("GET");
+			options.WithOrigins("https://localhost:5001");
+		});
+
+		//app.MapGet("/", () => "Hello World!");
+
+		app.MapGet("/api/weather", () =>
+		{
+			return Enumerable.Range(1, 5).Select(index =>
+			new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = WeatherForecast.Summaries[Random.Shared.Next(WeatherForecast.Summaries.Length)]
+			}).ToArray();
+		});
+
+		app.Run();
+	}
+}
