045f0b93d7229a26ab31a98e214a20612eb45016
Beneqi <obbonobo@gmail.com>
2023-12-10 17:50:48 +0600

    Abstracting a service 1

--- Northwind.BlazorServer/Data/INorthwindService.cs
+++ Northwind.BlazorServer/Data/INorthwindService.cs
@@ -1,6 +1,11 @@
-﻿namespace Northwind.BlazorServer.Data
+﻿namespace Coldons.Lib;
+
+public interface INorthwindService
 {
-    public interface INorthwindService
-    {
-    }
+	Task<List<Customer>> GetCustomersAsync();
+	Task<List<Customer>> GetCustomersAsync(string country);
+	Task<Customer?> GetCustomerAsync(string id);
+	Task<Customer> CreateCustomerAsync(Customer c);
+	Task<Customer> UpdateCustomerAsync(Customer c);
+	Task DeleteCustomerAsync(string id);
 }
--- Northwind.BlazorServer/Data/NorthwindService.cs
+++ Northwind.BlazorServer/Data/NorthwindService.cs
@@ -1,6 +1,58 @@
-﻿namespace Northwind.BlazorServer.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Coldons.Lib;
+
+public class NorthwindService : INorthwindService
 {
-	public class NorthwindService
+	private readonly NorthwindContext db;
+
+	public NorthwindService(NorthwindContext db)
 	{
+		this.db = db;
 	}
+
+	public Task<List<Customer>> GetCustomersAsync()
+	{
+		return db.Customers.ToListAsync();
+	}
+
+	public Task<List<Customer>> GetCustomersAsync(string country)
+	{
+		return db.Customers.Where(c => c.Country == country).ToListAsync();
+	}
+
+	public Task<Customer?> GetCustomerAsync(string id)
+	{
+		return db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+	}
+
+
+	public Task<Customer> CreateCustomerAsync(Customer c)
+	{
+		db.Customers.Add(c);
+		db.SaveChangesAsync();
+		return Task.FromResult(c);
+	}
+
+	public Task<Customer> UpdateCustomerAsync(Customer c)
+	{
+		db.Entry(c).State = EntityState.Modified;
+		db.SaveChangesAsync();
+		return Task.FromResult(c);
+	}
+
+	public Task DeleteCustomerAsync(string id)
+	{
+		Customer? customer = db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id).Result;
+		if (customer == null)
+		{
+			return Task.CompletedTask;
+		}
+		else
+		{
+			db.Customers.Remove(customer);
+			return db.SaveChangesAsync();
+		}
+	}
+
 }
