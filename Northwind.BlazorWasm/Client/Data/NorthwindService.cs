1b48fb34ec8f208953aa8486cce19155d33a15ec
Beneqi <obbonobo@gmail.com>
2023-12-11 19:27:55 +0600

    total ff

--- Northwind.BlazorWasm/Client/Data/NorthwindService.cs
+++ Northwind.BlazorWasm/Client/Data/NorthwindService.cs
@@ -1,6 +1,46 @@
-﻿namespace Northwind.BlazorWasm.Client.Data
+﻿using System.Net.Http.Json; // GetFromJsonAsync, ReadFromJsonAsync
+using Coldons.Lib; //Customer
+
+namespace Northwind.BlazorWasm.Client.Data;
+
+public class NorthwindService : INorthwindService
 {
-	public class NorthwindService
+	private readonly HttpClient http;
+	public NorthwindService(HttpClient http)
 	{
+		this.http = http;
+	}
+
+	public Task<List<Customer>> GetCustomersAsync()
+	{
+		return http.GetFromJsonAsync<List<Customer>>("api/customers");
+	}
+
+	public Task<List<Customer>> GetCustomersAsync(string country)
+	{
+		return http.GetFromJsonAsync<List<Customer>>($"api/customers/in/{country}");
+	}
+
+	public Task<Customer?> GetCustomerAsync(string id)
+	{
+		return http.GetFromJsonAsync<Customer>($"api/customers/{id}");
+	}
+
+	public async Task<Customer> CreateCustomerAsync(Customer c)
+	{
+		HttpResponseMessage response = await http.PostAsJsonAsync("api/customers", c);
+		return await response.Content.ReadFromJsonAsync<Customer>();
+	}
+
+	public async Task DeleteCustomerAsync(string id)
+	{
+		HttpResponseMessage response = await http.DeleteAsync($"api/customers/{id}");
+	}
+
+
+	public async Task<Customer> UpdateCustomerAsync(Customer c)
+	{
+		HttpResponseMessage response = await http.PutAsJsonAsync("api/customers", c);
+		return await response.Content.ReadFromJsonAsync<Customer>();
 	}
 }
